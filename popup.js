0d6e13a95173fee3d0f7774f9afaac402e795f83
l2fm <l2fm2005@gmail.com>
2025-01-27 21:42:15 -0400

    3.0

--- popup.js
+++ popup.js
@@ -1,4 +1,43 @@
-// function showPopup() {
-//     var popup = document.getElementById('btn_doacao');
-//     popup.classList.toggle('show');
-// }
+const voluntario = document.getElementById('popup_voluntario');
+const verTodos_doadores = document.getElementById('popup_doadores');
+const verTodos_voluntarios = document.getElementById('popup_voluntarios');
+const verTodos_projetos = document.getElementById('popup_projetos');
+const doacao = document.getElementById('popup_doacao');
+
+function showDoacaoPopup() {
+    doacao.showModal();
+}
+
+function closePopup() {
+    doacao.close();
+};
+function showVoluntarioPopup() {
+    voluntario.showModal();
+}
+
+function closePopup() {
+    voluntario.close();
+}
+
+
+function handleButtonClick(event) {
+    const buttonId = event.target.id;
+
+    switch (buttonId) {
+        case 'popup_voluntario':
+            voluntario.showModal();
+
+        case 'popup_doadores':
+            verTodos_doadores.showModal();
+
+        case 'popup_voluntarios':
+            verTodos_voluntarios.showModal();
+
+        case 'popup_projetos':
+            verTodos_projetos.showModal();
+
+        case 'popup_doacao':
+            doacao.showDoacaoPopup();
+
+    }
+}
