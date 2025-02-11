12acde2b71c4cae472152564e28f4d12c0922fd4
l2fm <l2fm2005@gmail.com>
2025-02-10 21:35:54 -0400

    2.3

--- popup.js
+++ popup.js
@@ -1,5 +1,22 @@
-const openPopup = document.getElementById('nome-projeto');
-const fundoPopup = document.getElementById('fundopopup');
+//Carousel//
+let currentIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+const totalItems = items.length;
+
+function changeSlide() {
+    currentIndex = (currentIndex + 1) % totalItems; 
+    const offset = -currentIndex * 100;
+
+    document.querySelector('.carousel-imgs').style.transform = `translateX(${offset}%)`; // Corrigido aqui
+}
+
+setInterval(changeSlide, 3000); 
+
+//fim carousel//
+
+// popup login //
+const openPopup = document.getElementById('btn-login');
+const fundoPopup = document.getElementById('fundo-popup');
 
 openPopup.addEventListener('click', () => {
     fundoPopup.classList.add('ativo');
@@ -10,3 +27,16 @@ fundoPopup.addEventListener('click', (event) => {
         fundoPopup.classList.remove('ativo');
     }
 });
+
+//fim popup login//
+
+//Voltar ao inicio//
+function voltarParaTelaInicial() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    setTimeout(function() {
+        window.location.href = 'tela-projeto.html';
+    }, 500);}
+    //Fim do voltar ao inicio3//
